fa3e332e1ffdeca61e7d642846ed5e52dea52bfa
Mariano Spallino <61815996+mars-86@users.noreply.github.com>
2021-06-09 21:40:14 -0300

    refac srt_split and str_replace (#1)

--- string/string_manip.c
+++ string/string_manip.c
@@ -3,37 +3,39 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define str_split_static(dest, dest_len, dest_str_len, src, delim) \
+{ \
+    char src_temp[strlen(src) + 1], *token; \
+    int i; \
+    sprintf(src_temp, "%s", src); \
+    for(i = 0, token = strtok(src_temp, delim); token != NULL; ++i) { \
+        sprintf(dest[i], "%s", token); \
+        token = strtok(NULL, delim); \
+    } \
+}
+
 int is_occurrence(const char *src, const char *occurrence)
 {
-    char *occ_p = (char *)occurrence, *src_p = (char *)src;
-    while (*occ_p != '\0')
-        if (*occ_p++ != *src_p++) return 0;
+    while (*occurrence != '\0')
+        if (*occurrence++ != *src++) return 0;
     return 1;
 }
 
 const char *str_replace(char *dest, const char *src, const char *r_text, const char *n_text)
 {
-    char *dest_p = dest, *src_p = (char *)src, *r_text_p = (char *)r_text, *n_text_p = (char *)n_text, *src_p_temp;
-    int eq;
+    char *dest_p = dest, src_temp[strlen(src) + 1], *r_text_p = (char *)r_text;
+    char *src_p = &src_temp[0], *n_text_p = (char *)n_text;
+    int r_text_len = strlen(r_text);
+    sprintf(src_temp, "%s", src);
     while (*src_p != '\0') {
-        if (*src_p == *r_text_p) {
-            src_p_temp = src_p, eq = 1;
-            while (*r_text_p != '\0')
-                // if replace text differs from source at any position
-                if (*r_text_p++ != *src_p++) {
-                    src_p = src_p_temp, eq = 0;
-                    *dest_p++ = *src_p++;
-                    break;
-                }
-            if (eq) {
+        if (*src_p == *r_text_p)
+            if (is_occurrence(src_p + 1, r_text_p + 1)) {
                 while (*n_text_p != '\0')
                     *dest_p++ = *n_text_p++;
-                n_text_p = (char *)n_text;
+                n_text_p = (char *)n_text, src_p += r_text_len;
+                continue;
             }
-            r_text_p = (char *)r_text;
-        } else {
-            *dest_p++ = *src_p++;
-        }
+        *dest_p++ = *src_p++;
     }
     *dest_p = '\0';
     return dest;
@@ -42,15 +44,14 @@ const char *str_replace(char *dest, const char *src, const char *r_text, const c
 char **str_split(char ***dest, const char *src, const char *delim)
 {
     int occur = str_occurrences(src, delim) + 2;
-    char *src_temp = (char *)malloc((strlen(src) + 1) * sizeof(char)), *token;
+    char src_temp[strlen(src) + 1], *token;
     char **dest_p = *dest = (char **)malloc(occur * sizeof(char *));
     sprintf(src_temp, "%s", src);
     for(token = strtok(src_temp, delim); token != NULL; token = strtok(NULL, delim), dest_p++) {
-        *dest_p = (char *)malloc(strlen(token) + 1 * sizeof(char));
+        *dest_p = (char *)malloc((strlen(token) + 1) * sizeof(char));
         sprintf(*dest_p, "%s", token);
     }
     *dest_p = NULL;
-    free(src_temp);
     return *dest;
 }
 
@@ -69,19 +70,9 @@ const char *str_capitalize(char *dest, const char *src, int from_idx, int to_idx
 
 int str_occurrences(const char *src, const char *occurrence)
 {
-    char *src_p = (char *)src, *occ_p = (char *)occurrence, *src_p_temp;
-    int i, eq;
-    for (i = 0; *src_p != '\0'; src_p++)
-        if (*src_p == *occ_p) {
-            src_p_temp = src_p, eq = 1;
-            while (*occ_p != '\0')
-                if (*occ_p++ != *src_p++) {
-                    src_p = src_p_temp, eq = 0;
-                    break;
-                }
-            if (eq) ++i;
-            occ_p = (char *)occurrence;
-            // if (is_occurrence(src_p + 1, occ_p + 1)) ++i;
-        }
+    int i;
+    for (i = 0; *src != '\0'; src++)
+        if (*src == *occurrence)
+            if (is_occurrence(src + 1, occurrence + 1)) ++i;
     return i;
 }
