6b2256d3da7efbb41566560efbd561dcf5b5945e
mars-86 <hrslash86@gmail.com>
2021-06-25 00:36:27 -0300

    fix tabs at common.h

--- template/common.h
+++ template/common.h
@@ -29,18 +29,18 @@
 
 #define _template_length_internal(_node_base_type, _this_node) \
     _node_base_type *_base_node_temp = (_node_base_type *)_this_node->start; \
-        int i = 0; \
-        while (_base_node_temp != NULL) \
-            ++i, _base_node_temp = _base_node_temp->next; \
-        return i;
+    int i = 0; \
+    while (_base_node_temp != NULL) \
+        ++i, _base_node_temp = _base_node_temp->next; \
+    return i;
 
 #define _template_remove_all_internal(_base_node_type, _this_node, _this_node_back_ptr) \
     if(_this_node->_this_node_back_ptr == NULL) return; \
-        _base_node_type *_base_node_temp = (_base_node_type *)_this_node->_this_node_back_ptr; \
-        while(_base_node_temp != NULL) { \
-            _this_node->_this_node_back_ptr = _base_node_temp->prev; \
-            _template_delete_node_internal(_base_node_temp); \
-            _base_node_temp = (_base_node_type *)_this_node->_this_node_back_ptr; \
-        } \
+    _base_node_type *_base_node_temp = (_base_node_type *)_this_node->_this_node_back_ptr; \
+    while(_base_node_temp != NULL) { \
+        _this_node->_this_node_back_ptr = _base_node_temp->prev; \
+        _template_delete_node_internal(_base_node_temp); \
+        _base_node_temp = (_base_node_type *)_this_node->_this_node_back_ptr; \
+    } \
 
 #endif // _TEMPLATE_COMMON_INCLUDED_H_
