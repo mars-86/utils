c926be30674cfeb298f3bf3576711cf12b991ad9
mars-86 <hrslash86@gmail.com>
2021-06-27 00:53:39 -0300

    move status codes to new file

--- networking/header.c
+++ networking/header.c
@@ -1,9 +1,11 @@
 #include "header.h"
+#include <stdio.h>
+#include <string.h>
 
 const char *generate_headers(const char *dest, HTTP_STATUS_CODES status, const char *body)
 {
     sprintf(dest,
-            "HTTP/1.1 %d OK\r\n"
+            "HTTP/1.1 %ull OK\r\n"
             "Content-Type: text/html\r\n"
             "Content-Length: %d\r\n\r\n"
             "%s", status, strlen(body), body);
