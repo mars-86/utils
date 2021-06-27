53473ad80055120bd7160ee1bf49bcb5fdeaddbf
mars-86 <hrslash86@gmail.com>
2021-06-27 00:42:49 -0300

    add http status codes

--- networking/header.c
+++ networking/header.c
@@ -1,6 +1,6 @@
 #include "header.h"
 
-const char *generate_headers(const char *dest, STATUS_CODES status, const char *body)
+const char *generate_headers(const char *dest, HTTP_STATUS_CODES status, const char *body)
 {
     sprintf(dest,
             "HTTP/1.1 %d OK\r\n"
