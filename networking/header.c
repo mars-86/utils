d76acc1f6a59b35d9d85af112e189be10613c76a
mars-86 <hrslash86@gmail.com>
2021-07-28 20:06:02 -0300

    fix data types

--- networking/header.c
+++ networking/header.c
@@ -5,9 +5,9 @@
 const char *generate_headers(char *dest, HTTP_STATUS_CODES status, const char *body)
 {
     sprintf(dest,
-            "HTTP/1.1 %lu OK\r\n"
-            "Content-Type: text/html\r\n"
-            "Content-Length: %lu\r\n\r\n"
+            "HTTP/1.1 %llu OK\r\n"
+            "Content-Type: text/HTML\r\n"
+            "Content-Length: %u\r\n\r\n"
             "%s", status, strlen(body), body);
     return dest;
 }
