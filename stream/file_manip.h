cc8935a20d8870cba2c314c7985e622b0386db41
Mariano Spallino <61815996+mars-86@users.noreply.github.com>
2021-09-01 23:31:55 -0300

    refac functions (#9)

--- stream/file_manip.h
+++ stream/file_manip.h
@@ -8,23 +8,27 @@ typedef struct _FILEBUF FILEBUF;
 typedef struct FILEBUF** FILEBUFL;
 
 enum _FILE_MANIP_MODES {
-	FILE_MANIP_READ,
-	FILE_MANIP_WRITE,
-	FILE_MANIP_APPEND
+	FILE_MANIP_READ = 0x01,
+	FILE_MANIP_WRITE = 0x02,
+	FILE_MANIP_APPEND = 0x04,
+	FILE_MANIP_PLUS = 0x08,
+	FILE_MANIP_BINARY = 0x10
 };
 
-int open_file(FILE** fd, const char* path, const char* mode);
-int open_file_fb(FILEBUF** fb, const char* path, int mode);
+int file_open(FILE** fd, const char* path, int mode);
+int file_open_fb(FILEBUF** fb, const char* path, int mode);
 int is_file_open(FILE* fd);
 int is_file_open_fb(FILEBUF* fb);
-size_t file_size(FILE* fd);
+long file_size(const char *path);
 size_t file_size_fb(FILEBUF* fb);
 const char file_mode_fb(FILEBUF* fb);
 const char file_type_fb(FILEBUF* fb);
-void* read_file(char* buff, long buff_size);
-long long read_binary_file(FILE* fd, unsigned char* fbuff, long buff_size);
-void* find_in_file(const char* search);
-int close_file(FILE* fd);
-int close_file_fb(FILEBUF* fb);
+void* file_read(char* buff, long buff_size);
+long long file_read_binary(FILE* fd, unsigned char* fbuff, long buff_size);
+void* file_find(const char* search);
+int file_create(const char *path, unsigned char *buff, size_t size);
+int file_dup(const char *path_dest, const char *path_src);
+int file_close(FILE* fd);
+int file_close_fb(FILEBUF* fb);
 
 #endif // !FILE_MANIP_H
